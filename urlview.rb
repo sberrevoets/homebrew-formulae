90d5818f4033808dab55b36a2910df3eb76923e2
Scott Berrevoets <s.berrevoets@me.com>
2016-02-06 22:15:11 -0800

    Add urlview

---
+++ urlview.rb
@@ -0,0 +1,18 @@
+class Urlview < Formula
+  desc "URL extractor/launcher"
+  homepage "https://github.com/sigpipe/urlview"
+  head "https://github.com/sberrevoets/urlview.git"
+
+  def install
+    inreplace "urlview.man", "/etc/urlview/url_handler.sh", "open"
+    inreplace "urlview.c",
+      '#define DEFAULT_COMMAND "/etc/urlview/url_handler.sh %s"',
+      '#define DEFAULT_COMMAND "open %s"'
+
+    man1.mkpath
+    ENV.prepend "CFLAGS", "-I /usr/local/include"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}",
+                          "--sysconfdir=#{etc}", "--with-pcre"
+    system "make", "install"
+  end
+end
